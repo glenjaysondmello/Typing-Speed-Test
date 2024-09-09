ffa4ad218dbcbae9a565335971f9a976e13cff12
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2024-09-09 12:29:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,12 +29,15 @@ function loadParagraph() {
 
   const randomIndex = Math.floor(Math.random() * paragraph.length);
   typingText.innerHTML = "";
-  for (const char of paragraph[randomIndex]) {
-    typingText.innerHTML += `<span>${char}</span>`;
-  }
-  typingText.querySelectorAll("span")[0].classList.add("active");
-  document.addEventListener("keydown", () => input.focus());
-  typingText.addEventListener("click", input.focus());
+  btn.addEventListener("click", () => {
+    typingText.innerHTML = "";
+    for (const char of paragraph[randomIndex]) {
+      typingText.innerHTML += `<span>${char}</span>`;
+    }
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => input.focus());
+    typingText.addEventListener("click", input.focus());
+  });
 }
 
 function initTyping(e) {
